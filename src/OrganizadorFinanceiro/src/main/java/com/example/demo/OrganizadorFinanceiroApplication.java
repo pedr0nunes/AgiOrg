d9c56f2906bc8774f7462962fb81b2761e7e73d1
fabichimura <39606605+xaolin342@users.noreply.github.com>
2021-12-01 12:40:56 -0300

    Adiciona metadata, e projeto com Login e Lista de Usuarios

---
+++ src/OrganizadorFinanceiro/src/main/java/com/example/demo/OrganizadorFinanceiroApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrganizadorFinanceiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrganizadorFinanceiroApplication.class, args);
+	}
+
+}
